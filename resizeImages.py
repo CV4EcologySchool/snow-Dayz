1b8b25bbf1ed97efffdc42bfd3b2bc6e2f84eea5
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-12 17:35:02 -0400

    resize alfkj

--- resizeImages.py
+++ resizeImages.py
@@ -3,9 +3,10 @@
 from PIL import Image
 import glob
 import os 
+import pandas as pd
 
-files = glob.glob('/datadrive/data/weather/train/*.JPG')
-newPath = '/datadrive/data/weather/train_resized/'
+files = glob.glob('/datadrive/vmData/weather/train/*.JPG')
+newPath = '/datadrive/vmData/weather/train_resized/'
  
 if not os.path.exists(newPath):
     os.makedirs(newPath)
@@ -14,7 +15,7 @@ for file in files:
     filename = file.spilt('/')[-1] ### last item
  # Opens a image in RGB mode
     im = Image.open(str(file))
-    newsize = (300, 300)
+    newsize = (500, 500)
     im1 = im.resize(newsize)
     # Shows the image in image viewer
     im1 = im1.save(newPath + filename)
