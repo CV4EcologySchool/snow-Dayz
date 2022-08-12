9e71e13cddb7bde105269b993d2989a748b9a1e9
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-12 19:48:21 -0400

    256e 256 batch

--- resizeImages.py
+++ resizeImages.py
@@ -3,22 +3,27 @@
 from PIL import Image
 import glob
 import os 
+import tqdm
 import pandas as pd
 
-files = glob.glob('/datadrive/vmData/weather/train/*.JPG')
+files = glob.glob('/datadrive/vmData/weather/train/*')
 newPath = '/datadrive/vmData/weather/train_resized/'
  
 if not os.path.exists(newPath):
     os.makedirs(newPath)
 
-for file in files: 
-    file = file
-    test = file[32:len(file)] #.spilt('/') ### last item
-    print(test)
- # Opens a image in RGB mode
-    filename = test
-    im = Image.open(str(file))
-    newsize = (500, 500)
-    im1 = im.resize(newsize)
-    # Shows the image in image viewer
-    im1 = im1.save(newPath + filename)
+for file in tqdm.tqdm(files): 
+    try:
+        test = file[32:len(file)] #.spilt('/') ### last item
+        # print(test)
+    # Opens a image in RGB mode
+        filename = test
+        new_filename = newPath + filename
+        if not os.path.exists(new_filename):
+            im = Image.open(str(file))
+            newsize = (500, 500)
+            im1 = im.resize(newsize)
+            # Shows the image in image viewer
+            im1 = im1.save(new_filename)
+    except Exception: 
+        pass
