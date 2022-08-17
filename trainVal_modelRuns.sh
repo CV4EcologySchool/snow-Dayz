c235df69bae9191f6e7e2dd8f0942a765175367c
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-17 10:34:44 -0400

    bash script

--- trainVal_modelRuns.sh
+++ trainVal_modelRuns.sh
@@ -13,7 +13,7 @@
 
 #python classifier/train.py --config configs/exp_resnet50_3classes.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_None  --train_folder train_resized --val_folder val_resized
 
-python classifier/train.py --config configs/exp_resnet50_3classes_seq6hr.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq6hr  --train_folder train_resized --val_folder val_resized
+#python classifier/train.py --config configs/exp_resnet50_3classes_seq6hr.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq6hr  --train_folder train_resized --val_folder val_resized
 
 python classifier/train.py --config configs/exp_resnet50_3classes_seq12hr.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq12hr --train_folder train_resized --val_folder val_resized
 
