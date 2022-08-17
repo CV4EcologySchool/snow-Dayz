dff01ea5b577b02f59a0bcae892d0ff2f6134666
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-16 23:53:39 -0400

    eval

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -193,7 +193,7 @@ def save_precision_recall_curve(true_labels, predicted_labels, cfg, args, epoch=
     PRcurve.plot()
     plt.savefig(cfg['data_root'] + '/experiments/'+(args.exp_name)+'/figs/PRcurve'+str(epoch)+'_'+ str(split) +'.png', facecolor="white")
 
-def binaryMetrics(cfg, dl_val, model, args):
+def binaryMetrics(cfg, dl_val, model, args, epoch):
     print('generating binary predicted labels')
     filenames, true_labels, predicted_labels, confidences = predict(cfg, dl_val, model)   
     print('done generating predicted labels')
@@ -236,7 +236,7 @@ def binaryMetrics(cfg, dl_val, model, args):
     results.to_csv(cfg['data_root'] + '/experiments/'+(exp_name)+'/figs/'+'results.csv')
     print("results csv saved")
 
-def multiClassMetrics(cfg, dl_val, model, args):
+def multiClassMetrics(cfg, dl_val, model, args, epoch):
     print('generating multi-class predicted labels')
     filenames, true_labels, predicted_labels, confidences0, confidences1, confidences2 = predict(cfg, dl_val, model)   
     print('done generating predicted labels')
@@ -286,11 +286,11 @@ def main():
 
     if cfg['num_classes'] == 2:
         print('calculating binary metrics')
-        binaryMetrics(cfg, dl_val, model, args)
+        binaryMetrics(cfg, dl_val, model, args, epoch)
 
     if cfg['num_classes'] == 3:
         print('calculating multiclass metrics')
-        multiClassMetrics(cfg, dl_val, model, args)
+        multiClassMetrics(cfg, dl_val, model, args, epoch)
 
 
 if __name__ == '__main__':
