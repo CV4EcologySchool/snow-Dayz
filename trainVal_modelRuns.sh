4186751950e4b4905debfbe8cd5a1a6bcb3afdcf
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-16 20:38:49 -0400

    fixed bash

--- trainVal_modelRuns.sh
+++ trainVal_modelRuns.sh
@@ -3,13 +3,13 @@
 
 #python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_2classes_None  --train_folder train_resized --val_folder val_resized
 
-python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq6hr  --train_folder train_resized --val_folder val_resized
+python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_2classes_seq6hr  --train_folder train_resized --val_folder val_resized
 
-python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq12hr --train_folder train_resized --val_folder val_resized
+python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_2classes_seq12hr --train_folder train_resized --val_folder val_resized
 
-python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seq24hr --train_folder train_resized --val_folder val_resized
+python classifier/train.py --config configs/exp_resnet50_2classes.yaml --exp_dir experiments --exp_name exp_resnet50_2classes_seq24hr --train_folder train_resized --val_folder val_resized
 
-python classifier/train.py --config configs/exp_resnet50_2classes_seqSliding.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_seqSliding --train_folder train_resized --val_folder val_resized
+python classifier/train.py --config configs/exp_resnet50_2classes_seqSliding.yaml --exp_dir experiments --exp_name exp_resnet50_2classes_seqSliding --train_folder train_resized --val_folder val_resized
 
 python classifier/train.py --config configs/exp_resnet50_3classes.yaml --exp_dir experiments --exp_name exp_resnet50_3classes_None  --train_folder train_resized --val_folder val_resized
 
