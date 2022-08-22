14862c28e9bd8842cf8fd9eadbea75bdd2b22f9a
Catherine Breen <catherine.m.breen@gmail.com>
2022-08-22 14:05:57 -0400

    ldsjf

--- resizeImages.py
+++ resizeImages.py
@@ -11,17 +11,18 @@ import pandas as pd
 #files = glob.glob('/datadrive/vmData/weather/train/*')
 #newPath = '/datadrive/vmData/weather/train_resized/'
 
-files = glob.glob('/datadrive/vmData/weather/test/*')
-newPath = '/datadrive/vmData/weather/test_resized/'
+#files = glob.glob('/datadrive/vmData/weather/test/*')
+#newPath = '/datadrive/vmData/weather/test_resized/'
  
-
+snoqfiles = glob.glob('/datadrive/vmData/snoq/**/*')
+newPath = '/datadrive/vmData/weather/snoq_resized/'
 
 #if not os.path.exists(newPath):
 #    os.makedirs(newPath)
 
 #files = glob.glob('/Volumes/CatBreen/CV4ecology/scandcam/Snow/*')
 
-for file in tqdm.tqdm(files): 
+for file in tqdm.tqdm(snoqfiles): 
     #print(file.split('/')[-1])
     try:
         #test = file[32:len(file)] #.spilt('/') ### last item file.split('/')
@@ -29,6 +30,34 @@ for file in tqdm.tqdm(files):
         # Opens a image in RGB mode
         filename = file.split('/')[-1]
         new_filename = newPath + filename
+        if not os.path.exists(new_filename):
+            im = Image.open(str(file))
+            newsize = (500, 500)
+            im1 = im.resize(newsize)
+            #im1.show()
+            # Shows the image in image viewer
+            im1 = im1.save(new_filename)
+    except Exception as e:
+        print(f"{e} {file}")
+        pass
+        
+#################################################################################
+olympexfiles = glob.glob('/datadrive/vmData/olympex/**/*')
+OLYnewPath = '/datadrive/vmData/weather/olympex_resized/'
+
+#if not os.path.exists(newPath):
+#    os.makedirs(newPath)
+
+#files = glob.glob('/Volumes/CatBreen/CV4ecology/scandcam/Snow/*')
+
+for file in tqdm.tqdm(olympexfiles): 
+    #print(file.split('/')[-1])
+    try:
+        #test = file[32:len(file)] #.spilt('/') ### last item file.split('/')
+            # print(test)
+        # Opens a image in RGB mode
+        filename = file.split('/')[-1]
+        new_filename = OLYnewPath + filename
         if not os.path.exists(new_filename):
             im = Image.open(str(file))
             newsize = (500, 500)
