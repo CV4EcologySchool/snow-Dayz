56c9b462f64a10631869eb3a6c022af55fe2b93a
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2023-06-06 11:06:11 +0200

    adjusted output path

--- snowpoles/src/config.py
+++ snowpoles/src/config.py
@@ -5,7 +5,7 @@ import os
 #OUTPUT_PATH = '/Volumes/CatBreen/CV4ecology/snow_poles_outputs_resized' #'/datadrive/vmData/snow_poles_outputs'
 
 ROOT_PATH = '/datadrive/vmData/SNEX20_TLI_resized'
-OUTPUT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe5_BS64_clean_wWAOK_IN_May30test'
+OUTPUT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe5_BS64_E100_clean_SNEX_IN'
 #OUTPUT_PATH = '/Users/catherinebreen/Documents/Chapter1/dendrite_outputs/IN/snow_poles_outputs_resized_LRe4_BS64_clean_wWAOK_IN'
 
 # learning parameters
