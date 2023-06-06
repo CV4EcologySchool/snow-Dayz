39d2bca865ae851cbcb38f187ccf567ba68ec2ce
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2023-06-06 15:00:18 +0200

    adjust LR

--- snowpoles/src/config.py
+++ snowpoles/src/config.py
@@ -5,12 +5,12 @@ import os
 #OUTPUT_PATH = '/Volumes/CatBreen/CV4ecology/snow_poles_outputs_resized' #'/datadrive/vmData/snow_poles_outputs'
 
 ROOT_PATH = '/datadrive/vmData/SNEX20_TLI_resized_clean'
-OUTPUT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe5_BS64_E100_clean_SNEX_IN'
+OUTPUT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe4_BS64_E100_clean_SNEX_IN'
 #OUTPUT_PATH = '/Users/catherinebreen/Documents/Chapter1/dendrite_outputs/IN/snow_poles_outputs_resized_LRe4_BS64_clean_wWAOK_IN'
 
 # learning parameters
 BATCH_SIZE = 64 #64 #4 #32
-LR = 0.00001  # #0.0001 lower to factor of 10
+LR = 0.0001 #0.00001  # #0.0001 lower to factor of 10
 EPOCHS = 100 #100
 #DEVICE = torch.device('mps')  #should be cuda on VMs
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
