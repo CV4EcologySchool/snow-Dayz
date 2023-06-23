a82f74387541544521bed7e85b2f2567578a5d53
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2023-06-23 13:55:15 +0200

    fixed batch size

--- snowpoles/src/config.py
+++ snowpoles/src/config.py
@@ -23,7 +23,7 @@ native_res_path = '/datadrive/vmData/nativeRes.csv'
 #OUTPUT_PATH = '/Users/catherinebreen/Documents/Chapter1/dendrite_outputs/IN/snow_poles_outputs_resized_LRe4_BS64_clean_wWAOK_IN'
 
 # learning parameters
-BATCH_SIZE = 4 #64 #4 #32
+BATCH_SIZE = 64 #64 #4 #32
 LR = 0.00001 #0.00001  # #0.0001 lower to factor of 10
 EPOCHS = 100 #100
 #DEVICE = torch.device('mps')  #should be cuda on VMs
