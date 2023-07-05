eba9e91a0f575b0c4089ff5ac595a2ee9a36a0ce
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2023-07-05 14:07:51 +0200

    15 p camera

--- snowpoles/src/config.py
+++ snowpoles/src/config.py
@@ -40,4 +40,4 @@ keypointColumns = ['x1', 'y1', 'x2', 'y2'] ## update
 # Fine-tuning set-up
 FINETUNE = True ## True for test/val dataset to be the subset 
 FT_PATH = '/datadrive/vmData/snow_poles_outputs_resized_LRe4_BS64_E100_clean_SNEX_IN' ## model that you want to fine tune
-FT_sample = 20
+FT_sample = 15
