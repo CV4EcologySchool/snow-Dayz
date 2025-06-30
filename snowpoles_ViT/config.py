c53bdef3470a6ef32ef7ebf523bc792b013fe9ea
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2025-06-30 15:22:08 -0400

    co-wa-ak split

--- snowpoles_ViT/config.py
+++ snowpoles_ViT/config.py
@@ -15,5 +15,5 @@ learning_rate = 1e-4
 weight_decay = 0.001
 
 #output_path = "/Users/catherinebreen/Dropbox/snowpoles_ViT_outputs/ViT_bs6_10p"
-output_path = "/datadrive/vmData/snowpoles_ViT_outputs/ViT_bs64_co-wa-wasplit"
+output_path = "/datadrive/vmData/snowpoles_ViT_outputs/ViT_bs64_co-wa-aksplit"
 
